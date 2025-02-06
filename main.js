e74a807ec966bffad3a78730ef72d04549c789fa
Gosse <gosse.shelter@gmail.com>
2025-02-06 01:33:35 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 var counter = 0;
 const counter_box = document.getElementById('counter');
-console.log(counter_box);
+const my_audio = document.getElementsByClassName('meow');
+
+function playing(){
+    var num = Math.floor((Math.random() * my_audio.length));
+    console.log(num)
+    my_audio[num].play();
+}
+
 function on_click() {
     counter++;
     counter_box.innerHTML = `<h1 class="score"> ${counter} </h1>`;
-    console.log(counter);
+    //console.log(counter);
+    playing();
+    
 }
